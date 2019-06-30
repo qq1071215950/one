6a65bfb16f69b7de49be98db8b1fdea1ea980273
root123 <1071215950@qq.com>
2019-06-30 22:01:03 +0800

    new test

---
+++ src/main/java/cn/cm/test/Test.java
@@ -0,0 +1,7 @@
+package cn.cm.test;
+
+public class Test {
+    public static void main (String[] args){
+           System.out.println("this is a test,in order to test the code accoding to the git");
+    }
+}
